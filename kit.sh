d3eaf9d91dffeff2e2c09a82e79eb00cee6a586e
andreidopastel <130707844+andreidopastel@users.noreply.github.com>
2026-08-19 09:46:24 -0400

    kit.sh

--- kit.sh
+++ kit.sh
@@ -14,7 +14,7 @@ echo -e "${V}---- FERRAMENTA DE REDE ----${NC}"
 # Seleção de Alvo
 echo -ne "\nAlvo do teste? Padrão 143.0.36.20: "
 read -t 10 RESP
-TARGET=${RESP:-143.0.36.38}
+TARGET=${RESP:-143.0.36.20}
 [[ "$TARGET" == "y" || -z "$TARGET" ]] && TARGET="143.0.36.20"
 
 # [1] RASTREIO DE ROTA INTELIGENTE (PARA NO ALVO)
